cde2ec792da7350b2e4a0b6b11b67da131e58a9e
mickeyyo55b <mickeyyo55b@gmail.com>
2025-01-10 11:51:37 +0800

    01101151

--- brad99.php
+++ brad99.php
@@ -1,43 +1,31 @@
+<?php
+    define("ROWS", 1);
+    define("START", 1);
+    define("COLS", 9);
+?>
 <h1>Brad Big Company</h1>
 <hr />
+<form action="brad99.php">
+    Start: <input type="number" name="start" />
+    Rows: <input type="number" name="rows" />
+    Columns: <input type="number" name="cols" />
+    <input type="submit" value="Change" />
+</form>
 <table width='100%' border='1'>
-    <tr>
-        <?php
-            for ($j = 2; $j <= 5; $j++){
+    <?php
+        for ($k = 0; $k < ROWS; $k++){
+            echo '<tr>';
+            for ($j = START; $j < START+COLS; $j++){
+                $newj = $j + $k * COLS;
                 echo '<td>';
                 for ($i = 1; $i<= 9; $i++){
-                    $r = $j * $i;
-                    echo "{$j} x {$i} = {$r}<br />";
+                    $r = $newj * $i;
+                    echo "{$newj} x {$i} = {$r}<br />";
                 }
                 echo '</td>';   
             }
-        ?>
-    </tr>
-    <tr>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-    </tr>
-
+            echo '</tr>';
+    
+        }
+    ?>
 </table>
