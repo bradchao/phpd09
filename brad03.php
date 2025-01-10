470b5826efa8a99e9b4b94176bbd1c6345f3f173
mickeyyo55b <mickeyyo55b@gmail.com>
2025-01-10 08:41:28 +0800

    init

---
+++ brad03.php
@@ -0,0 +1,9 @@
+<?php
+    // 弱型別
+    $var1 = 123;
+    $var1 = 3.21;
+    $var1 = 'Brad';
+
+    echo gettype($var1);
+
+?>
---
+++ brad04.php
@@ -0,0 +1,10 @@
+<?php
+    $a = '110'; $b = '123';
+    $c = $a + $b;
+    $text = "$a + $b = $c<br />\n";
+    echo $text;
+
+    $w = 80.4;
+    echo "Brad weight = {$w}kg";
+
+?>
