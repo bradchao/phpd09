017cabf91e8d5c95e3bacd873610440f3f2e763e
mickeyyo55b <mickeyyo55b@gmail.com>
2025-01-10 16:25:56 +0800

    01101625

--- pokerv4.php
+++ pokerv4.php
@@ -2,9 +2,9 @@
     $poker = range(0,51);
     shuffle($poker);
 
-    foreach ($poker as $card){
-        echo "{$card}<br />";
-    }
+    // foreach ($poker as $card){
+    //     echo "{$card}<br />";
+    // }
 ?>
 <hr />
 <?php
@@ -13,8 +13,21 @@
         $players[$i%4][(int)($i/4)] = $card;
     }
 
-    foreach($players[1] as $card){
-        echo "{$card}<br />";
-    }
-
 ?>
+<table width='100%' border='1'>
+    <?php
+        $flowers = ['&spades;','<font color="red">&hearts;</font>',
+                    '<font color="red">&diams;</font>','&clubs;'];
+        $values = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+        foreach($players as $player){
+            sort($player);
+            echo '<tr>';
+            foreach($player as $card){
+                $flower = $flowers[(int)($card / 13)];
+                $value = $values[$card % 13];
+                echo "<td>{$flower}{$value}</td>";
+            }
+            echo '</tr>';
+        }
+    ?>
+</table>
