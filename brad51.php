58bc4980d8e9aaa1094892b8802807a654840da7
mickeyyo55b <mickeyyo55b@gmail.com>
2025-01-15 14:02:10 +0800

    01151402

--- brad51.php
+++ brad51.php
@@ -1,7 +1,32 @@
 <?php
-    if (!isset($_GET['account'])) header('Location: brad50.html');
-
+    if (!isset($_REQUEST['account'])) header('Location: brad50.html');
     
+    $mysqli = new mysqli('localhost','root','', 'brad');
+    $mysqli->set_charset('utf8');
+
+    $account = $_REQUEST['account'];
+    $passwd = password_hash($_REQUEST['passwd'], PASSWORD_DEFAULT);
+    $realname = $_REQUEST['realname'];
+    $icon = $_FILES['icon'];
+    if ($icon['error'] == 0){
+        $iconData = file_get_contents($icon['tmp_name']);
+        $iconType = $icon['type'];
+        $sql = 'INSERT INTO cust (account,passwd,realname,icon,icontype)' . 
+                ' VALUES (?,?,?,?,?)';
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('sssss', $account, $passwd, $realname, $iconData, $iconType);
+    }else{
+        $sql = 'INSERT INTO cust (account,passwd,realname) VALUES (?,?,?)';
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('sss', $account, $passwd, $realname);
+    }
+    
+    if ($stmt->execute()){
+        header('Location: brad52.php');
+    }else{
+        echo "ERROR: {$mysqli->error}";
+    }
+
 
 
 ?>
