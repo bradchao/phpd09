eb65011d35beb80e29e25a79999f0d3048755ba8
mickeyyo55b <mickeyyo55b@gmail.com>
2025-01-15 09:15:16 +0800

    01150915

--- brad44.php
+++ brad44.php
@@ -1,5 +1,12 @@
 <?php
     $passwd = '123456';
     $hashPasswd = password_hash($passwd, PASSWORD_DEFAULT);
-    echo $hashPasswd;
+    //echo $hashPasswd;
+
+    if (password_verify('123456', $hashPasswd)){
+        echo 'OK';
+    }else{
+        echo 'XX';
+    }
+
 ?>
