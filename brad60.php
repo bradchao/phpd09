7de52d76588808e4c3c2d1ece4b9ded029f4912e
mickeyyo55b <mickeyyo55b@gmail.com>
2025-01-16 11:11:34 +0800

    01161111

--- brad60.php
+++ brad60.php
@@ -7,6 +7,7 @@
         WHERE od.OrderID = 10248        
 
         {
+            'error': 0,     
             'cid': 'VINET',
             'eid' : 5,
             'date': '1996-07-04',
@@ -29,7 +30,42 @@
                     ]
         }
     */
+    if (!isset($_REQUEST['orderId'])){
+        $respont = ['error' => 1];
+    }else{
+        $orderId = $_REQUEST['orderId'];
+        $mysqli = new mysqli('localhost','root','', 'brad');
+        $mysqli->set_charset('utf8');
+        $sql = 'SELECT o.CustomerID, o.EmployeeID, o.OrderDate, p.ProductName, od.UnitPrice, od.Quantity
+                FROM orderdetails od
+                JOIN orders o ON (o.OrderID = od.OrderID)
+                JOIN products p ON (p.ProductID = od.ProductID)
+                WHERE od.OrderID = ?';
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('i', $orderId);
+        if (!$stmt->execute()){
+            $respont = ['error' => 2];
+        }else{
+            $stmt->store_result();
+            if ($stmt->num_rows == 0){
+                $respont = ['error' => 3];
+            }else{
+                $respont = ['error' => 0];
+                $stmt->bind_result($cid,$eid,$date,$pname,$price,$qty);
+                while ($stmt->fetch()){
+                    $respont['cid'] = $cid;
+                    $respont['eid'] = $eid;
+                    $respont['date'] = $date;
+                    $respont['detail'][] = [
+                        'pname' => $pname,
+                        'price' => $price,
+                        'qty' => $qty
+                    ];
+                }
+            }
+        }
+    }
 
-
-
+    header('Content-type: application/json');
+    echo json_encode($respont);
 ?>
