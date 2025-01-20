81058a7cbe995783e4c101c3ed6e05d1a4025585
mickeyyo55b <mickeyyo55b@gmail.com>
2025-01-20 11:24:25 +0800

    01201124

--- brad68.php
+++ brad68.php
@@ -25,8 +25,108 @@
         public function getAuthor(){return $this->author;}
 
         public function getDetails(){
-            return "Name:{$this->name};Price:{$this->price};Author:{$this->author}";}
+            return "Name:{$this->name};Price:{$this->price};Author:{$this->author}";
+        }
     }
+
+    class Seafood extends Product {
+        private $kind;
+
+        public function __construct($name, $price, $kind){
+            $this->name = $name;
+            $this->price = $price;
+            $this->kind = $kind;
+        }
+
+        public function getKind(){return $this->kind;}
+
+        public function getDetails(){
+            return "Name:{$this->name};Price:{$this->price};Kind:{$this->kind}";
+        }
+    }
+    //-----------------------------------------
+    // 介面 interface => 規格
+    interface DiscountStrategy{
+        public function setDiscount($price);
+    }
+
+    class FixedDiscount implements DiscountStrategy {
+        private $amount;
+        public function __construct($amount){
+            $this->amount = $amount;
+        }
+        public function setDiscount($price){
+            return max(1,$price - $this->amount);
+        }
+    }
+
+    class PercentageDiscount implements DiscountStrategy {
+        private $percentage;    // 50% => 50
+        public function __construct($percentage){
+            $this->percentage = $percentage;
+        }
+        public function setDiscount($price){
+            return $price * $this->percentage / 100;
+        }
+    }
+    //------------------------------
+    class Cart {
+        private $items = [];    // element(item) is-a Product
+        private $discountStrategy = null;  // 不打折 => null
+
+        public function addItem(Product $product){
+            $this->items[] = $product;
+        }
+
+        public function setDiscountStrategy(DiscountStrategy $strategy){
+            $this->discountStrategy = $strategy;
+        }
+
+        public function calcTotal(){
+            $total = 0;
+            foreach($this->items as $item){
+                $price = $item->getPrice();
+                if ($this->discountStrategy != null){
+                    $price = $this->discountStrategy->setDiscount($price);
+                }
+                $total += $price;
+            }
+
+            return $total;
+        }
+
+        public function listItems(){
+            foreach($this->items as $item){
+                echo "{$item->getDetails()}<br />";
+            }
+        }
+    }
+    //--------------------------------------------
+
+    $book1 = new Book('PHP 大全', 300, 'Brad');
+    $book2 = new Book('MySQL 大全', 400, 'Pig');
+    $fish = new Seafood('fish', 100, 'fish1');
+
+    $cart = new Cart();
+    $cart->addItem($book1);
+    $cart->addItem($book2);
+    $cart->addItem($fish);
+
+    $cart->listItems();
+    echo "Price1: {$cart->calcTotal()}<br />";
+
+    $cart->setDiscountStrategy(new FixedDiscount(10));
+    echo "Price2: {$cart->calcTotal()}<br />";
+
+    $cart->setDiscountStrategy(new PercentageDiscount(90));
+    echo "Price3: {$cart->calcTotal()}<br />";
+
+
+
+
+
+
+
 
 
 ?>
